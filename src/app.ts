969acb046c0937de1c6d667bc85e25d7dd77704f
AR-Jame <ar.jame333@gmail.com>
2025-10-28 02:54:51 +0600

    Server setup done

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Application, Request, Response } from 'express'
+import cors from "cors";
+import config from './config';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
+
+
+const app: Application = express();
+
+
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/api/v1", router)
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({
+        message: "Server is running..",
+        environment: config.node_env,
+        uptime: process.uptime().toFixed(2) + " second.",
+        time: new Date().toISOString()
+    })
+})
+
+
+app.use(globalErrorHandler)
+
+app.use(notFound);
+
+export default app
